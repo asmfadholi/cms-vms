739416fddbe1d301b2582224ab723238ec95082c
Mochammad Fadholi <36128632+asmfadholi@users.noreply.github.com>
2021-11-25 21:36:09 +0700

    chore: revise port env

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,6 +15,9 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
     ]
   },
+  server: {
+    host: '0.0.0.0' // default: localhost
+  },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
